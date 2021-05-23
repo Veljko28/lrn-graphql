8de4c5816396f4144ec92780a47132584ca9fe2f
Veljko28 <charlibear28@gmail.com>
2021-05-23 21:30:29 +0200

    Student and Class type added

--- index.js
+++ index.js
@@ -3,14 +3,36 @@ const {ApolloServer, gql} = require('apollo-server');
 const typeDefs = gql`
  type Query {
     hello: String!
+    student: Student!
+ }
+
+ type Student { 
+     id: ID!
+     name: String!
+     schoolClass: SchoolClass
+ }
+
+ type SchoolClass {
+     id: ID!
+     subject: String!
+     students: [Student!]
  }
 
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "Hello World"
-    }
+        hello: () => "Hello World",
+        student: () => ({
+            id: () => "1",
+            name: () => "Veljko",
+            schoolClass: () => ({
+                id: () => "1",
+                subject: () => "Programming",
+                students: []
+            })
+        })
+    },
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
