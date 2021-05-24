071362f2f581b077db779688d950046da854aa54
Veljko28 <charlibear28@gmail.com>
2021-05-24 21:14:21 +0200

    Adding login with register error

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const {ApolloServer, gql} = require('apollo-server');
 
+const users = [
+    {
+    name: "test",
+    password: "test"
+    },
+    {
+        name: "test",
+        password: "test"
+    }
+];
+
+
 const typeDefs = gql`
  type Query {
     hello: String!
     student: Student!
+    getAllUsers: [UserInfo!]
  }
 
  type Student { 
@@ -16,6 +29,17 @@ const typeDefs = gql`
      id: ID!
      subject: String!
      students: [Student!]
+ }
+
+
+ input UserInfo {
+    name: String!
+    password: String!
+ }
+
+ type Mutation {
+     login(info: UserInfo!): Boolean!
+     register(info: UserInfo!): UserInfo!
  }
 
 `;
@@ -31,8 +55,22 @@ const resolvers = {
                 subject: () => "Programming",
                 students: []
             })
-        })
+        }),
+        getAllUsers: () => users
     },
+
+    Mutation: {
+        login: (parent,args) => {
+            const user = users.filter(x => x.name === args.info.name)[0];
+            if (user === null)
+                return false;
+            return user.password === args.info.password;    
+        },
+        register: (parent,args) => {
+            users.push(args.info);
+            return args.info;
+        }
+    }
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
