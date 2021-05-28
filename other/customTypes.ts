d36367664cbefe5038948ffd6003fa8a60778a70
Veljko28 <charlibear28@gmail.com>
2021-05-28 15:30:43 +0200

    reslover map type created

---
+++ other/customTypes.ts
@@ -0,0 +1,11 @@
+
+export interface ResolverMap {
+    [key: string]: {
+        [key:string]: (
+            parent: any,
+            args: any,
+            context: any,
+            info: any
+        ) => any;
+    }; 
+}
