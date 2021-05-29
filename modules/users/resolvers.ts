d3d48899f21242a41e6cfcd7daac91b322d25cea
Veljko28 <charlibear28@gmail.com>
2021-05-29 17:03:41 +0200

    adding yup validation to register

--- modules/users/resolvers.ts
+++ modules/users/resolvers.ts
@@ -1,5 +1,12 @@
 import {User} from  '../../models/User';
 import {ResolverMap} from  '../../other/customTypes';
+import {formatYupError} from '../../other/formatYupError';
+import * as yup from 'yup';
+
+const yupSchema = yup.object().shape( {
+    email: yup.string().min(10).max(255).email(),
+    password: yup.string().min(6).max(255)
+})
 
 
 export const resolvers: ResolverMap = {
@@ -15,10 +22,17 @@ export const resolvers: ResolverMap = {
     },
 
     Mutation: {
-        register: async (_: any, {info: { email,password } }: {info: {email: string,password: string}}) => {
+        register: async (_: any, args: {info: {email: string,password: string}}) => {
+            const {info: { email,password } } = args;
+            try {
+                await yupSchema.validate(args, { abortEarly: false });
+              } catch (err) {
+                return formatYupError(err);
+              }
+            
             const user = new User({email,password});
             await user.save();
-            return user;
+            return null;
         },
 }
 }
