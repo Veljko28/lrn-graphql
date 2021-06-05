e5f82f40720b3d71549981e2a48b330f2a40dd8a
Veljko28 <charlibear28@gmail.com>
2021-06-05 16:30:26 +0200

    adding redis to context

--- other/MyContext.ts
+++ other/MyContext.ts
@@ -1,8 +1,10 @@
 import { Response, Request } from "express";
+import { Redis }from 'ioredis';
 
 export interface MyContext {
     req: Request;
     res: Response;
+    redis: Redis,
     payload?: {
         UserId: string,
         used: Boolean
